28f106c83d8ff999d47989863b9e8f21390b5002
Esedov Muhammed <esedovmuhammed0@gmail.com>
2025-01-11 15:46:18 +0400

    feat: frontend allow

--- balance_management/src/main/java/az/developia/balance_management/config/CorsConfig.java
+++ balance_management/src/main/java/az/developia/balance_management/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500/", "https://balance-management-production.up.railway.app")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization")
