ebd30fa76acb2a7ca7cf9a29fec7c4c2bd6657eb
Esedov Muhammed <esedovmuhammed0@gmail.com>
2025-01-11 16:07:52 +0400

    feat: frontend allow

--- balance_management/src/main/java/az/developia/balance_management/config/CorsConfig.java
+++ balance_management/src/main/java/az/developia/balance_management/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://balance-management001.netlify.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization")
